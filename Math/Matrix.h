99540d8a44249855d3d5cdd5db772206ed7c6ec6
krtk92 <44470768+krtk92@users.noreply.github.com>
2018-10-26 06:23:50 -0400

    matrix changed to two-dimensional

--- Math/Matrix.h
+++ Math/Matrix.h
@@ -9,15 +9,15 @@ public:
         size_t m_row{0};
         size_t m_column{0};
     public:
-        Index(size_t row, size_t column);
+		Index(size_t row = 0, size_t column = 0);
+		Index(const std::pair<size_t, size_t> pos);
 
         size_t row() const {return m_row;}
         size_t column() const {return m_column;}
-        size_t mult() const {return m_row * m_column; }
-        size_t calc_raw_index(const Index& in) const;
+        size_t mult() const {return m_row * m_column;}
     };
 
-    Matrix(const std::vector<value_t>& arr, size_t columns);
+	Matrix(const std::vector<std::vector<Matrix::value_t>> &arr, size_t row, size_t columns);
 
     Matrix(size_t row, size_t column, value_t default_val = 0.0);
 
@@ -42,6 +42,8 @@ public:
     friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
 private:
     Index m_index;
-    std::vector<value_t> m_values;
+	size_t cols;
+	size_t row;
+    std::vector<std::vector<value_t>> m_values;
 
 };
