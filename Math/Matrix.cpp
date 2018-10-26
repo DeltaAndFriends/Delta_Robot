7face531cd198cc52707e9a64cfe51acbe3f3b12
krtk92 <44470768+krtk92@users.noreply.github.com>
2018-10-26 06:21:23 -0400

    fixed some old errors, added some new errors

--- Math/Matrix.cpp
+++ Math/Matrix.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
+#include <vector>
 #include <stdexcept>
 #include "Matrix.h"
 
-Matrix::Matrix(const std::vector<Matrix::value_t> &arr, size_t columns)
-    : m_index{arr.size() / columns, columns}
-    , m_values(arr)
-{
-    if (m_values.size() != m_index.mult()) {
-        throw std::invalid_argument("Matrix::Matrix(): Matrices aren't of the same size!");
-    }
-}
+
+Matrix::Matrix(const std::vector<std::vector<Matrix::value_t>> &arr, size_t _row, size_t columns)
+    : row(_row), cols(columns), m_values(arr)
+{}
 
 Matrix::Matrix(size_t row, size_t column, value_t default_val)
     : m_index{row, column}
-    , m_values(m_index.mult(), default_val)
 {
+	m_values = std::vector < std::vector<value_t>>(row, std::vector<value_t>(cols, 0));
 }
 
 Matrix::Matrix(const Matrix &other)
@@ -25,9 +22,14 @@ Matrix::Matrix(const Matrix &other)
 
 Matrix& Matrix::operator+=(const Matrix& other)
 {
-    if (m_index.row() == other.m_index.row() && m_index.column() == other.m_index.column()) {
-        for(size_t i{0}; i < m_values.size(); ++i) {
-            m_values[i] += other.m_values[i];
+    if (m_index.row() == other.m_index.row() && m_index.column() == other.m_index.column()) 
+	{
+		for(size_t i{0}; i < m_values.size(); ++i) 
+		{
+			for (size_t j{ 0 }; j < m_values[0].size(); ++j)
+			{
+				m_values[i][j] += other.m_values[i][j];
+			}
         }
         return *this;
     }
@@ -37,9 +39,13 @@ Matrix& Matrix::operator+=(const Matrix& other)
 
 Matrix& Matrix::operator-=(const Matrix& other) {
     if (m_index.row() == other.m_index.row() && m_index.column() == other.m_index.column()) {
-        for(size_t i{0}; i < m_values.size(); ++i) {
-            m_values[i] -= other.m_values[i];
-        }
+        for(size_t i{0}; i < m_values.size(); ++i) 
+		{
+			for (size_t j{ 0 }; j < m_values[0].size(); ++j)
+			{
+				m_values[i][j] -= other.m_values[i][j];
+			}
+			}
         return *this;
     }
 
@@ -50,7 +56,10 @@ Matrix Matrix::operator+(const Matrix& other) {
     if (m_index.row() == other.m_index.row() && m_index.column() == other.m_index.column()) {
         Matrix result{*this};
         for(size_t i{0}; i < m_values.size(); ++i) {
-            result.m_values[i] += other.m_values[i];
+			for (size_t j{ 0 }; j < m_values[0].size(); ++j)
+			{
+				result.m_values[i][j] += other.m_values[i][j];
+			}
         }
         return result;
     }
@@ -64,7 +73,7 @@ Matrix Matrix::operator*(const Matrix& other) const {
         for (size_t i = 1; i <= result.m_index.row(); ++i) {
             for (size_t j = 1; j <= result.m_index.column(); ++j) {
                 for (size_t k = 1; k <= m_index.column(); ++k) {
-                    result[Index{i, j}] += (*this)[Index{i, k}] * other[Index{k, j}];
+                    result[{i, j}] += (*this)[{i, k}] * other[{k, j}];
                 }
             }
         }
@@ -75,11 +84,11 @@ Matrix Matrix::operator*(const Matrix& other) const {
 }
 
 Matrix::value_t& Matrix::operator[](const Index& index) {
-    return m_values[m_index.calc_raw_index(index)];
+    return m_values[index.row][index.column]; //TODO something with this shit
 }
 
 Matrix::value_t Matrix::operator[](const Index& index) const {
-    return m_values[m_index.calc_raw_index(index)];
+    return m_values[index.row][index.column];
 }
 
 Matrix& Matrix::operator=(const Matrix& other) {
@@ -92,28 +101,30 @@ Matrix& Matrix::operator=(const Matrix& other) {
 }
 
 std::ostream& operator<<(std::ostream& os, const Matrix& m) {
-   for (size_t i = 0; i < m.m_index.mult(); i++) {
-       if ((i % m.m_index.column() == 0) && i != 0) os << std::endl;
-       os << m.m_values[i] << ' ';
+   for (size_t i = 0; i < m.m_values.size(); ++i) {
+	   for (size_t j = 0; j < m.m_values[0].size(); ++j)
+	   {
+		   os << m.m_values[i][j] << ' ';
+	   }
+	   os << std::endl;
    }
    return os;
+}
+
+Matrix::Index::Index(const std::pair<size_t, size_t> pos)
+	: m_row{ pos.first }
+	, m_column{ pos.second }
+{
+	if (m_row < 0 || m_column < 0) {
+		throw std::invalid_argument("Matrix::Index::Index: index cannot be 0");
+	}
 }
 
 Matrix::Index::Index(size_t row, size_t column)
     : m_row{row}
     , m_column{column}
 {
-    if (!m_row || !m_column) {
+    if (m_row < 0 || m_column < 0) {
         throw std::invalid_argument("Matrix::Index::Index: index cannot be 0");
     }
-}
-
-size_t Matrix::Index::calc_raw_index(const Matrix::Index &in) const
-{
-    if (m_row && m_column
-            && m_row >= in.m_row && m_column >= in.m_column)
-    {
-        return (in.m_row - 1) * m_column + in.m_column - 1;
-    }
-    throw std::invalid_argument("Matrix::Index::calc_raw_index: invalid index");
 }
